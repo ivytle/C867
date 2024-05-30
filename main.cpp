ee09f25d2ea3b2979818265855ba19cbf8f97336
ivytle <86812443+ivytle@users.noreply.github.com>
2024-05-30 14:34:00 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ const int ROSTERSIZE = 5;
 const int DATASIZE = 9;
 const string studentData[ROSTERSIZE] = {
     "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY", "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK", "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE", "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-    "A5,Ivy  ,Le    ,ile5@wgu.edu,23,24,40,45,SOFTWARE"
+    "A5,Ivy  ,Le    ,xxxx@wgu.edu,23,24,40,45,SOFTWARE"
 };
 
 void F1(){
